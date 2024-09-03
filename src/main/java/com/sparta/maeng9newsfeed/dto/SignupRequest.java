2229810a312e68a8242225383d244ab9e09ae990
Sangmin Lee <sart5534@gmail.com>
2024-09-03 13:10:59 +0900

    feat: 회원 가입
    회원가입 시 Bearer 토큰을 Authorization 헤더에 포함하여 반환

---
+++ src/main/java/com/sparta/maeng9newsfeed/dto/SignupRequest.java
@@ -0,0 +1,11 @@
+package com.sparta.maeng9newsfeed.dto;
+
+import lombok.Getter;
+
+@Getter
+public class SignupRequest {
+
+    private String userName;
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/sparta/maeng9newsfeed/dto/SignupResponse.java
@@ -0,0 +1,13 @@
+package com.sparta.maeng9newsfeed.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class SignupResponse {
+
+    private final String bearerToken;
+
+
+}
