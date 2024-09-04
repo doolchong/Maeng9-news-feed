55b6ee7eae02d72a2f41556bc489877bb7342c3a
Sangmin Lee <sart5534@gmail.com>
2024-09-04 15:13:44 +0900

    feat: 비밀번호 수정

--- src/main/java/com/sparta/maeng9newsfeed/controller/UserController.java
+++ src/main/java/com/sparta/maeng9newsfeed/controller/UserController.java
@@ -1,16 +1,11 @@
 package com.sparta.maeng9newsfeed.controller;
 
 import com.sparta.maeng9newsfeed.annotation.Auth;
-import com.sparta.maeng9newsfeed.dto.AuthUser;
-import com.sparta.maeng9newsfeed.dto.UserResponse;
-import com.sparta.maeng9newsfeed.dto.UserUpdateRequest;
-import com.sparta.maeng9newsfeed.dto.UserUpdateResponse;
+import com.sparta.maeng9newsfeed.dto.*;
 import com.sparta.maeng9newsfeed.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -29,5 +24,13 @@ public class UserController {
     public UserUpdateResponse updateUser(@Auth AuthUser authUser, @RequestBody UserUpdateRequest userUpdateRequest) {
         Long userId = authUser.getId();
         return userService.updateUser(userId, userUpdateRequest);
+    }
+
+    @PatchMapping("/password")
+    public ResponseEntity<String> updatePassword(@Auth AuthUser authUser,
+                                                 @RequestBody PasswordChangeRequest passwordChangeRequest) {
+        Long userId = authUser.getId();
+        userService.updatePassword(userId, passwordChangeRequest);
+        return ResponseEntity.ok("비밀번호가 변경되었습니다.");
     }
 }
