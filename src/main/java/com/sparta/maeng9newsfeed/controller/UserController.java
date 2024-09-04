45783a91cf802f3498b749b800db9d8906dca470
Sangmin Lee <sart5534@gmail.com>
2024-09-04 12:27:53 +0900

    feat: 프로필 수정

--- src/main/java/com/sparta/maeng9newsfeed/controller/UserController.java
+++ src/main/java/com/sparta/maeng9newsfeed/controller/UserController.java
@@ -3,9 +3,13 @@ package com.sparta.maeng9newsfeed.controller;
 import com.sparta.maeng9newsfeed.annotation.Auth;
 import com.sparta.maeng9newsfeed.dto.AuthUser;
 import com.sparta.maeng9newsfeed.dto.UserResponse;
+import com.sparta.maeng9newsfeed.dto.UserUpdateRequest;
+import com.sparta.maeng9newsfeed.dto.UserUpdateResponse;
 import com.sparta.maeng9newsfeed.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -21,4 +25,9 @@ public class UserController {
         return userService.getUser(userId);
     }
 
+    @PutMapping("/users")
+    public UserUpdateResponse updateUser(@Auth AuthUser authUser, @RequestBody UserUpdateRequest userUpdateRequest) {
+        Long userId = authUser.getId();
+        return userService.updateUser(userId, userUpdateRequest);
+    }
 }
--- src/main/java/com/sparta/maeng9newsfeed/service/UserService.java
+++ src/main/java/com/sparta/maeng9newsfeed/service/UserService.java
@@ -1,10 +1,13 @@
 package com.sparta.maeng9newsfeed.service;
 
 import com.sparta.maeng9newsfeed.dto.UserResponse;
+import com.sparta.maeng9newsfeed.dto.UserUpdateRequest;
+import com.sparta.maeng9newsfeed.dto.UserUpdateResponse;
 import com.sparta.maeng9newsfeed.entity.User;
 import com.sparta.maeng9newsfeed.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +18,14 @@ public class UserService {
     public UserResponse getUser(long userId) {
         User user = userRepository.findById(userId).orElseThrow(() -> new NullPointerException("해당 사용자를 찾을 수 없습니다."));
         return UserResponse.userResponse(user, 0);
+    }
+
+    @Transactional
+    public UserUpdateResponse updateUser(long userId, UserUpdateRequest userUpdateRequest) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new NullPointerException("해당 사용자를 찾을 수 없습니다."));
+
+        user.update(userUpdateRequest);
+
+        return new UserUpdateResponse(user.getUserName(), user.getEmail(), userUpdateRequest.getIntro());
     }
 }
